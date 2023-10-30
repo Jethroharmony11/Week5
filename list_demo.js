786d1bc9de80a325f9330b0d4fda4e9d7be810b1
Harmony Chofor <jethroharmony11@gmail.com>
2023-10-30 11:36:04 -0500

    arrays

---
+++ list_demo.js
@@ -0,0 +1,21 @@
+// ARRAY AKA list/Vector prdered seriers of Values
+pets = ["Biancan", "Finneley", "Dany",]
+console.log(pets[2]) //DANY 2 from the beginning
+
+let people = [
+    {
+        name: "Kevin",
+        age: 56
+},
+{
+    name: "Nina",
+    age: 47
+  }
+];
+  //ADD ONE MORE
+  peoplel.push ({name: "Kay", age: 59})
+
+// for(START, STOP, HOW)
+for(let index = 0; index < people.length; idex++){
+    console.log(people[index].name, people[index].age);
+}
