ad6d65097c2087671608cd9a9237a31f29456e7c
Harmony Chofor <jethroharmony11@gmail.com>
2023-10-31 08:40:28 -0500

    list

---
+++ collection.js
@@ -0,0 +1,16 @@
+//COLLECTION HOLD MULTIPLE VALUES
+// Array hold ordered lists of value, accessedby offset position (start at 0)
+let list = []
+
+list.push("kevin") // 0 index
+list.push("Nina") // 1 index
+list.push("Dandy") // 2 index
+
+let dog = list[2] //copy the value t index 2
+console.log(dog) //Danny
+
+lis[2] = "Biaca"
+
+console.log(list[2] );
+
+console.log()
