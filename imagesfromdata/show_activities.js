dfbfd719425139982c5dae049a58b113e518bff6
Harmony Chofor <jethroharmony11@gmail.com>
2023-11-03 15:35:01 -0500

    project

---
+++ imagesfromdata/show_activities.js
@@ -0,0 +1,39 @@
+console.log("Hello")
+console.log(categories[0])
+
+
+
+catch.innerHTML+=
+
+
+
+
+if(activities[i].category==e.Target.value){
+  act.innerHTML+=`<option value="${activies[i].id}"
+}
+
+
+
+
+
+
+extra.innerHTML=`
+for(i=0;i<activities.lenght;i++){
+    if()
+}
+
+
+
+
+
+
+         if (activities[i].price > 0) {
+             Buy.style.display="block"
+         }
+
+    }}else{
+        buy.style.display="none"
+    }
+}
+act.onchange = showExtra
+
---
+++ loops.js
@@ -0,0 +1,20 @@
+count = 10
+while(count > 0){
+    console.log(count);
+    count--;
+}
+console.log("BLAST OFF")
+
+for(i=0;i>0;i--){
+    console.log(i)
+}
+console.log("BLAST OF")
+
+locationst = ["apple", "banana", "cherry", "zebra"];
+
+for (i=list.lenght-1; i < 0; 1--){
+    console.log(list[i])
+}
+
+//TRADITIONAL LOOP FORWARDS
+for(i=0; i < list.lenght; i++)
